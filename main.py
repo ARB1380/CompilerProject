b77b96609a10ee671fd6232ba197bd7ef078571e
ARB1380 <azerila.farshi@gmail.com>
2023-03-31 12:55:45 +0430

    implement number and identifier

--- main.py
+++ main.py
@@ -1,16 +1,68 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import re
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def isSymbolOrBlank(input):
+    return input == ' ' or input == ';' or input == ':' \
+        or input == ',' or input == '[' or input == ']' \
+        or input == '(' or input == ')' or input == '{' \
+        or input == '}' or input == '+' or input == '-' \
+        or input == '*' or input == '=' or input == '<'
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def isSymbol(input):
+    return input == ';' or input == ':' or input == ',' \
+        or input == '[' or input == ']' or input == '(' \
+        or input == ')' or input == '{' or input == '}' \
+        or input == '+' or input == '-' or input == '*' \
+        or input == '=' or input == '<'
+
+def isNumber(input):
+    return re.search("[0-9]+", input) is not None
+
+def isIdentifier(input):
+    return re.search("[a-zA-Z][a-zA-Z0-9]*",input) is not None
+
+
+def get_token(left,right,input):
+    subString = input[left: right]
+    if isNumber(subString):
+        return f"(NUM,{subString})"
+    if isIdentifier(subString):
+        return f"(ID,{subString})"
+
+
+
+
+def get_next_token(input):
+    length = len(input)
+    left = 0
+    right = 0
+    while left <= right and right <= length:
+        if right != length and not(isSymbolOrBlank(input[right])):
+            right = right + 1
+        else:
+            if left == right:
+                if left == length:
+                    break
+                if isSymbolOrBlank(input[right]):
+                    print(f"symbol recognized!{input[right]}")
+                right = right + 1
+                left = right
+            else:
+                token = get_token(left,right,input)
+                print(token)
+                left = right
+
+
+
+
+
+input ="+="
+get_next_token(input)
+
+
+# file = open("input.txt","r")
+# counter = 1
+# for line in file:
+#     get_next_token(line)
