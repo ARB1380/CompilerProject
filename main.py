3238a494e50fb3d77ac2f7da0fa1edfd8652af35
ARB1380 <azerila.farshi@gmail.com>
2023-03-31 17:24:18 +0430

    refactor

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 import re
 
 
+class Token:
+    def __init__(self, type, lexeme):
+        self.type = type
+        self.lexeme = lexeme
+
+
+class Error:
+    def __init__(self, value, message):
+        self.value = value
+        self.message = message
+
 
 def is_symbol_or_blank(input):
     return input == ' ' or input == ';' or input == ':' or input == ',' \
         or input == '[' or input == ']' or input == '(' or input == ')'\
         or input == '{' or input == '}' or input == '+' or input == '-' \
         or input == '=' or input == '*' or input == '<'
+
 
 def is_symbol(input):
     return input == ';' or input == ':' or input == ',' \
@@ -15,48 +27,58 @@ def is_symbol(input):
         or input == '+' or input == '-' or input == '*' \
         or input == '=' or input == '<'
 
+
 def is_number(input):
     return re.search("^[0-9]+$", input) is not None
 
+
 def is_identifier(input):
-    return re.search("[a-zA-Z][a-zA-Z0-9]*",input) is not None
+    return re.search("[a-zA-Z][a-zA-Z0-9]*", input) is not None
+
 
 def is_keyword(input):
     return input == "if" or input == "else" or input == "void"\
         or input == "int" or input == "repeat" or input == "break" \
         or input == "until" or input == "return"
 
+
 def is_invalid_number(input):
     return re.search("[0-9]+[a-zA-Z][0-9a-zA-Z]*", input) is not None
 
-def is_invalid_input(input):
-    return re.search("[a-zA-Z0-9]*[^a-zA-Z0-9\s]", input) is not None
 
-def get_next_token(input, right, subString):
-    if is_symbol(subString):
-        if subString == '=' and input[right + 1] == '=':
-            return "(SYMBOL, ==)"
-        return f"(SYMBOL,{subString})"
-    if is_number(subString):
-        return f"(NUM,{subString})"
-    if is_invalid_number(subString):
-        return f"({subString}, invalid number)"
-    if is_keyword(subString):
-        return f"(KEYWORD,{subString})"
-    if is_identifier(subString):
-        return f"(ID,{subString})"
-    if is_invalid_input(subString):
-        return f"({subString}, invalid input)"
+def is_invalid_input(input):
+    return re.search("[a-zA-Z0-9]*[^a-zA-Z0-9\\s]", input) is not None
+
+
+def get_next_token(input, right, sub_string):
+    if is_symbol(sub_string):
+        if sub_string == '=' and input[right + 1] == '=':
+            return Token("SYMBOL", "==")
+        return Token("SYMBOL", sub_string)
+
+    if is_number(sub_string):
+        return Token("NUM", sub_string)
+
+    if is_keyword(sub_string):
+        return Token("KEYWORD", sub_string)
+
+    if is_identifier(sub_string):
+        return Token("ID", sub_string)
+
+    if is_invalid_number(sub_string):
+        return Error(sub_string, "Invalid number")
+
+    if is_invalid_input(sub_string):
+        return Error(sub_string, "Invalid input")
+
     return None
 
 
 def get_sub_string(left, right, input):
-    subString = input[left: right]
-    if len(subString) == 0:
-        subString = input[right]
-    return subString
-
-
+    sub_string = input[left: right]
+    if len(sub_string) == 0:
+        sub_string = input[right]
+    return sub_string
 
 
 def tokenize(input):
@@ -70,9 +92,9 @@ def tokenize(input):
             if left == right == length:
                 break
 
-            subString = get_sub_string(left, right, input)
-            token = get_next_token(input, right, subString)
-            if token == "(SYMBOL,==)":
+            sub_string = get_sub_string(left, right, input)
+            token = get_next_token(input, right, sub_string)
+            if token is Token and token.lexeme == "==":
                 left = left + 1
                 right = right + 1
 
@@ -82,10 +104,7 @@ def tokenize(input):
             left = right
 
 
-
-
-
-input ="cde = a;"
+input = "cde = a;"
 tokenize(input)
 
 
